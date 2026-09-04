7c0f2a851c9bd91ba0901b7f7aa520c8ea6aa07d
ayeshashahid19 <ayesha.shahid.20062003@gmail.com>
2026-09-05 01:30:46 +0500

    Remove admin categories page, fix SSL for Neon

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -5,7 +5,7 @@ import { PrismaPg } from '@prisma/adapter-pg'
 const globalForPrisma = globalThis
 
 function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL
   const isLocalHost = /localhost|127\.0\.0\.1|::1/.test(new URL(connectionString).hostname)
   const adapter = new PrismaPg({
     connectionString,
